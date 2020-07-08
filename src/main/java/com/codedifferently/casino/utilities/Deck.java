9d8d139b5946c2d769dbdfbbd3a71ba85501ba71
KeseanaH <45104070+KeseanaH@users.noreply.github.com>
2020-07-08 12:23:40 -0400

    added method

--- src/main/java/com/codedifferently/casino/utilities/Deck.java
+++ src/main/java/com/codedifferently/casino/utilities/Deck.java
@@ -1,15 +1,17 @@
 package com.codedifferently.casino.utilities;
-<<<<<<< HEAD
 
 /**
  * @author Keseana Howard
  */
 
-=======
-/**
- * @author Juliana Paul
- */
->>>>>>> 66f1a569ca748e71e40de366a483e61f9ef6a390
 public class Deck {
-    
+
+    public void add(){
+        int number = 0;
+
+        for(int i = 0; i < 5; i++)
+        {
+            newNumber = i;
+        }
+    }
 }
