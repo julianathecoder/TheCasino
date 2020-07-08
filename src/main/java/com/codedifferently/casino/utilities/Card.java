7becdb333765724355e6f3309b8899efbeb29ec7
KeseanaH <45104070+KeseanaH@users.noreply.github.com>
2020-07-08 19:46:33 -0400

    completed card class and created enum classes

--- src/main/java/com/codedifferently/casino/utilities/Card.java
+++ src/main/java/com/codedifferently/casino/utilities/Card.java
@@ -1,32 +1,14 @@
 package com.codedifferently.casino.utilities;
 
+import com.codedifferently.casino.utilities.cardenums.Suit;
+import com.codedifferently.casino.utilities.cardenums.Rank;
+import com.codedifferently.casino.utilities.cardenums.Color;
+
 /**
  * @author Keseana Howard
  */
 
-public class Card {
-
-    public enum Rank{
-        Ace(1), DUECE(2), THREE(3), 
-        FOUR(4), FIVE(5), SIX(6), 
-        SEVEN(7), EIGHT(8), NINE(9), 
-        TEN(10), JACK(10), QUEEN(10), 
-        KING(10);
-    
-        private final int rankValue;
-    
-        private Rank(int rankValue){
-            this.rankValue = rankValue; 
-        }
-
-        public int getRank(){ 
-            return rankValue; 
-        }
-    }
-
-    public enum Suit{CLUBS, DIAMONDS, HEARTDS, SPADES}
-    public enum Color{BLACK, RED}
-    
+public class Card {    
 
     private Suit suit;
     private Rank rank;
--- src/test/java/com/codedifferently/casino/utilities/CardTest.java
+++ src/test/java/com/codedifferently/casino/utilities/CardTest.java
@@ -1,14 +1,18 @@
 package com.codedifferently.casino.utilities;
 
+import com.codedifferently.casino.utilities.cardenums.Suit;
+import com.codedifferently.casino.utilities.cardenums.Rank;
+import com.codedifferently.casino.utilities.cardenums.Color;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class CardTest {
+public class CardTest{
     
     @Test
     public void constructorTest(){
-        Suit givenSuit = DIAMONDS;
-        Rank givenRank = 11;
+        Suit givenSuit = Suit.DIAMONDS;
+        Rank givenRank = Rank.FIVE;
         Color givenColor = Color.RED;
 
         Card cardTest = new Card(givenSuit, givenColor, givenRank);
@@ -16,6 +20,41 @@ public class CardTest {
         Suit retrieveSuit = cardTest.getSuit();
         Rank retrieveRank = cardTest.getRank();
         Color retrieveColor = cardTest.getColor();
+
+        Assert.assertEquals(givenSuit, retrieveSuit);
+        Assert.assertEquals(givenRank, retrieveRank);
+        Assert.assertEquals(givenColor, retrieveColor);
+    }
+
+    @Test
+    public void getSuitTest(){
+
+        Card cardTest = new Card(Suit.CLUBS, Color.RED, Rank.KING);
+
+        Suit actual = cardTest.getSuit();
+        Suit expected = Suit.CLUBS;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getRankTest(){
+
+        Card cardTest = new Card(Suit.HEARTS, Color.BLACK, Rank.DUECE);
+
+        Rank actual = cardTest.getRank();
+        Rank expected = Rank.DUECE;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getColorTest(){
+
+        Card cardTest = new Card(Suit.SPADES, Color.BLACK, Rank.ACE);
+
+        Color actual = cardTest.getColor();
+        Color expected = Color.BLACK;
 
         Assert.assertEquals(expected, actual);
     }
