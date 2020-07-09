c2364f7f5315198800447a0a699f1698ecb0e4c8
Juliana Paul <paul.juliana15@gmail.com>
2020-07-09 15:54:43 -0400

    Finished Dealer class

--- src/test/java/com/codedifferently/casino/utilities/DealerTest.java
+++ src/test/java/com/codedifferently/casino/utilities/DealerTest.java
@@ -1,5 +1,21 @@
 package com.codedifferently.casino.utilities;
 
+import org.junit.Test;
+
+/**
+ * @author Juliana Paul
+ */
+
 public class DealerTest {
+
+    @Test
+    public void pullFromDeckTest(){
+        
+    }
+
+    @Test
+    public void dealCardTest(){
+
+    }
     
 }
