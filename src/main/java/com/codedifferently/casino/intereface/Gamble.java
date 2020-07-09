1d4294b6b22d590346d2c0a9431776197414d067
Aldo <aalmeidarojo18@students.desu.edu>
2020-07-09 18:42:27 -0400

    fixed typo

--- src/main/java/com/codedifferently/casino/intereface/Gamble.java
+++ src/main/java/com/codedifferently/casino/intereface/Gamble.java
@@ -3,7 +3,7 @@ package com.codedifferently.casino.intereface;
 import com.codedifferently.casino.utilities.Player;
 
 public interface Gamble {
-    public void win();
-    public void lose();
+    public void win(Player player);
+    public void lose(Player player);
     public void bet(Player player,double money);
 }
