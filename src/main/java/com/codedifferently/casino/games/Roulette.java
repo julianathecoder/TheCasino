6e8b67947cdc3299ca3737186db8530a93a09016
Filiberto Camarillo <filibertocamarillo124@gmail.com>
2020-07-09 10:52:01 -0500

    Added and Removed 2 classes

--- src/main/java/com/codedifferently/casino/games/Roulette.java
+++ src/main/java/com/codedifferently/casino/games/Roulette.java
@@ -1,5 +1,46 @@
 package com.codedifferently.casino.games;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
+import com.codedifferently.casino.intereface.Gamble;
+import com.codedifferently.casino.intereface.Game;
+import com.codedifferently.casino.utilities.BallResults;
+import com.codedifferently.casino.utilities.Player;
+
+
+
+
+
 public class Roulette {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        int money = 1000;
+
+        Scanner player = new Scanner(System.in);
+        System.out.println("Please enter bet.");
+
+
+        int betMoney = player.nextInt();
+
+        int bet = Math.min(betMoney, money);
+            
+            int spinNumber = r.nextInt(37);
+            //int spinColor = 
+
+            if (spinNumber != 0 && spinNumber <= 18) {
+                money = money + bet;
+            } else {
+                money = money - bet;
+            }
+
+            System.out.println("betting $" + bet + ", spin is " + spinNumber + ", money now = $" + money);
+            System.out.println("Would you want to spin again?");
+
+        player.close();
+    }
     
+
 }
