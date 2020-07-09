8f5f862d5d14b42f07d25d9f4bab467aebca09eb
KeseanaH <45104070+KeseanaH@users.noreply.github.com>
2020-07-09 14:09:05 -0400

    started go fish

--- src/main/java/com/codedifferently/casino/games/GoFish.java
+++ src/main/java/com/codedifferently/casino/games/GoFish.java
@@ -1,5 +1,39 @@
 package com.codedifferently.casino.games;
 
-public class GoFish {
+import java.util.HashMap;
+
+import com.codedifferently.casino.utilities.Card;
+import com.codedifferently.casino.utilities.Player;
+
+/**
+ * @author Keseana Howard
+ */
+
+public class GoFish{
     
+    //private HashMap<Player, Card[]> hmap = new HashMap<Player, Card[]>();
+        
+    public GoFish(){
+        
+    }
+
+    public boolean ask(){
+        return false;
+    }
+
+    public boolean won(){
+        return false;
+    }
+
+    public boolean checkHand(){
+        return false;
+    }
+
+    public void giveCards(){
+
+    }
+
+    public Card publicFromDeck(){
+        return null;
+    }
 }
